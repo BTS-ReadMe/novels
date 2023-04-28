6e4638a5d2648cffb16b796cb3497aac0957dd7e
logwns <logwns@gmail.com>
2023-04-28 10:58:16 +0900

    first commit

---
+++ src/main/java/com/readme/novels/NovelsApplication.java
@@ -0,0 +1,15 @@
+package com.readme.novels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class NovelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelsApplication.class, args);
+	}
+
+}

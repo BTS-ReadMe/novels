8b74f8596587a9ab080da33e317d79be677bf135
Ju-Yeongmin <drodudals1@gmail.com>
2023-05-01 14:14:59 +0900

    [CREATE] health_check add

--- src/main/java/com/readme/novels/controller/EpisodesController.java
+++ src/main/java/com/readme/novels/controller/EpisodesController.java
@@ -1,13 +1,23 @@
 package com.readme.novels.controller;
 
 import com.readme.novels.model.Episodes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/novels-service")
+@RequiredArgsConstructor
 public class EpisodesController {
+
+    private final Environment env;
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's Working in User Service on PORT : %s"
+            , env.getProperty("local.server.port"));
+    }
 
     @GetMapping("/episodes")
     public Episodes getEpisode() {

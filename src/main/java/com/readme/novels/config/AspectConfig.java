83b389a1e9d730e2b04f8b99eaee66015a220e7a
Ju-Yeongmin <drodudals1@gmail.com>
2023-06-02 10:38:22 +0900

    [UPDATE] #BTS-151 prometheus 설정
    - prometheus, actuator 디펜던시 추가
    - TimedAspect를 사용하여 Aop로 microMeter 일괄적용

---
+++ src/main/java/com/readme/novels/config/AspectConfig.java
@@ -0,0 +1,9 @@
+package com.readme.novels.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfig {
+}

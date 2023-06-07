5cc4c033aacf7e00a4cd054a7605b4ccb267e9b5
Ju-Yeongmin <drodudals1@gmail.com>
2023-06-07 11:11:05 +0900

    [CREATE] #BTS-166 EpisodeHistory 수정
    - 기존 : 소설별로 가장 최근1개만 저장
    - 변경 : 읽은 내역에 모든 에피소드 저장,
     내역 조회할 때 가장 소설별로 가장 최근 1개만 조회

--- src/main/java/com/readme/novels/episodes/repository/EpisodeHistoryRepository.java
+++ src/main/java/com/readme/novels/episodes/repository/EpisodeHistoryRepository.java
@@ -1,10 +1,14 @@
 package com.readme.novels.episodes.repository;
 
+import com.readme.novels.episodes.dto.EpisodeRecentHistoryDto;
 import com.readme.novels.episodes.model.EpisodeHistory;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EpisodeHistoryRepository extends JpaRepository<EpisodeHistory, Long> {
     Optional<EpisodeHistory> findByUuidAndNovelId(String uuid, long novelId);
@@ -14,4 +18,10 @@ public interface EpisodeHistoryRepository extends JpaRepository<EpisodeHistory, 
     Boolean existsByIdAndUuid(Long id, String uuid);
 
     Optional<EpisodeHistory> findByUuidAndEpisodeId(String uuid, long episodeId);
+
+    @Query("SELECT new com.readme.novels.episodes.dto.EpisodeRecentHistoryDto(e.novelId, MAX(e.episodeId)) " +
+        "FROM EpisodeHistory e " +
+        "WHERE e.uuid = :uuid " +
+        "GROUP BY e.novelId")
+    Page<EpisodeRecentHistoryDto> findRecentEpisodeIdsByUuid(@Param("uuid") String uuid, Pageable pageable);
 }

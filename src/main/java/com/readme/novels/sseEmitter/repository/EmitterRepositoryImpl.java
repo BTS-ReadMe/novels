3ac90d45ecd840427f38d855bb450f67de5a82c7
logwns <logwns@gmail.com>
2023-06-08 21:49:09 +0900

    [CREATE] #BTS-170 : 에피소드 조회할 때 emitter 리턴

--- src/main/java/com/readme/novels/sseEmitter/repository/EmitterRepositoryImpl.java
+++ src/main/java/com/readme/novels/sseEmitter/repository/EmitterRepositoryImpl.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Repository
 @RequiredArgsConstructor
-public class SseEmitterRepository implements EmitterRepository {
+public class EmitterRepositoryImpl implements EmitterRepository {
 
     private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
 

575a20de86d2b9197ef23ada9b8629b96ad2491a
logwns <logwns@gmail.com>
2023-06-08 21:49:27 +0900

    [CREATE] #BTS-170 : 이모지 변경 상태 전송

---
+++ src/main/java/com/readme/novels/messageQueue/consumer/GetEmojiStatusConsumer.java
@@ -0,0 +1,28 @@
+package com.readme.novels.messageQueue.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.readme.novels.messageQueue.dto.EmojiStatusDto;
+import com.readme.novels.sseEmitter.repository.EmitterRepository;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
+@RequiredArgsConstructor
+public class GetEmojiStatusConsumer {
+
+    private final EmitterRepository emitterRepository;
+
+    @KafkaListener(id = "emojiStatus", topics = "emojiStatus")
+    public void listen(String kafkaMessage) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        EmojiStatusDto emojiStatusDto = mapper.readValue(kafkaMessage, EmojiStatusDto.class);
+
+        SseEmitter emitter = emitterRepository.findById(emojiStatusDto.getEpisodeId());
+        String message = mapper.writeValueAsString(emojiStatusDto);
+        emitter.send(SseEmitter.event().data(message).name("emojiStatus"));
+    }
+}
---
+++ src/main/java/com/readme/novels/messageQueue/dto/EmojiStatusDto.java
@@ -0,0 +1,11 @@
+package com.readme.novels.messageQueue.dto;
+
+import lombok.Getter;
+
+@Getter
+public class EmojiStatusDto {
+
+    private long episodeId;
+    private int episodeRow;
+    private int emoji;
+}

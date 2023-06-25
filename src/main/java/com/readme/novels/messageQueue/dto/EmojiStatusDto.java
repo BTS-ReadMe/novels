ed077aaebabcb2ae10a594560eb15b75be8e59ca
logwns <logwns@gmail.com>
2023-06-25 17:33:27 +0900

    [UPDATE] #BTS-170 : 실시간 접속 유저 수 확인 - 연결 된 유저 확인 후 dto에 연결되어있는 유저 수를 담아 전송했습니다

--- src/main/java/com/readme/novels/messageQueue/dto/EmojiStatusDto.java
+++ src/main/java/com/readme/novels/messageQueue/dto/EmojiStatusDto.java
@@ -1,13 +1,16 @@
 package com.readme.novels.messageQueue.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @ToString
 public class EmojiStatusDto {
 
     private long episodeId;
     private int episodeRow;
     private int emoji;
+    private int totalUser;
 }
